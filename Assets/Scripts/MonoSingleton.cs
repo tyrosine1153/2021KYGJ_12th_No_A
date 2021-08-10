d1d59c3780f45703373580b4c37ee96fd88e6196
tyrosine1153 <tyrosine1153@gmail.com>
2021-08-11 01:22:35 +0900

    Add Singleton Script
    MonoSingleton : 씬 내에서만 있는 싱글턴
    PersistentSingleton : 씬 전체에 있는 싱글턴

---
+++ Assets/Scripts/MonoSingleton.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public bool dontDestroyOnLoad;
+
+    protected static volatile T _instance;
+    protected static object _syncRoot = new System.Object();
+
+    public static T Instance {
+        get {
+            Initialize();
+            return _instance;
+        }
+    }
+
+    public static bool IsInitialized => _instance != null;
+
+    public static void Initialize()
+    {
+        if (_instance != null)
+        {
+            return;
+        }
+        lock (_syncRoot)
+        {
+            _instance = FindObjectOfType<T>();
+
+            if(_instance == null)
+            {
+                var go = new GameObject(typeof(T).FullName);
+                _instance = go.AddComponent<T>();
+            }
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance != null)
+        {
+            Debug.LogError(GetType().Name + " Singleton class is already created.");
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (_instance == this)
+        {
+            _instance = null;
+        }
+    }
+
+    protected virtual void OnAwake() { }
+}
---
+++ Assets/Scripts/PersistentSingleton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PersistentSingleton<T> : MonoSingleton<T> where T : MonoBehaviour
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        if (dontDestroyOnLoad)
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        OnAwake();
+    }
+}
